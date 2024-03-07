23ce09af3325864087924b9b90c0ae5bc1f1a95b
auspy <vinayak.kshetez0102@gmail.com>
2024-03-07 13:19:56 +0530

    refactor: setup colors, tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,44 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+
+const colors = {
+  primary: " #0052fe",
+  blueIcon: " #0082ff",
+  highlight: " #0141cf",
+  blueBg: " ##e8f4fd",
+  blueBgHero: " #e2ecfe",
+  gradBlue: " linear-gradient(81.62deg, #2870ea 8.72%, #1b4aef 85.01%)",
+
+  bg: "#EFF2F5",
+  heading: " #0f1629",
+  text: " #44475b",
+  textDark: " #111827",
+  textLight: " #788f9b",
+  subH: " #768396",
+  infoIcon: " #7c7e8c",
+  grey: " #c7c8ce",
+  headingSenti: " #191c1f",
+  textSenti: " #3e5765",
+  headingToken: " #202020",
+  textToken: " #3e424a",
+  subHHero: " #5d667b",
+
+  green: " #0fba83",
+  textGreen: " #14b079",
+  greenIcon: " #0fba83",
+  textGreenLike: " #32be88",
+  bgGreenLike: " #0ab27d0f",
+  greenBg: " #ebf9f4",
+  gradGreen: " linear-gradient(135deg, #79f1a4 0%, #0e5cad 100%)",
+
+  red: " #f7324c",
+  textRed: " #e96975",
+  bgRed: " #ee68551a",
+
+  gradOrange: " linear-gradient(135.73deg, #ff9865 0%, #ef3031 110.55%)",
+  orange: " #faa002",
+};
+
+export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,11 +46,7 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors,
     },
   },
   plugins: [],
